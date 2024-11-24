3e96f7022be60785bed6e180e7e22548e8bbf285
SHUBHAM SIDDHARTH <shubhamsiddharth123@gmail.com>
2024-11-24 21:46:19 +0530

    Initial commit

---
+++ Nov/ex_05112024_python/Lab003_Python_Basics.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Shubham Siddharth")
+
+#print (self, *args, sep=' ', end='\n', file=None):
+
+print("Shubham Siddharth",123,"AMIT", "John")
+
+print("Shubham Siddharth",123,"AMIT", "John", sep='*')
+print("Shubham Siddharth",123,"AMIT", "John", sep='*', end ="__")
+
+print("Shubham Siddharth",123,"AMIT", "John")
