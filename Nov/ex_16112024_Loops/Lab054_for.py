48f636cc3360b5bdf32f03fca7e071ea81a4da26
DELL <shubhamsiddharth123@gmail.com>
2024-12-02 13:49:47 +0530

    16 Nov first part

---
+++ Nov/ex_16112024_Loops/Lab054_for.py
@@ -0,0 +1,5 @@
+for i in range(1, 10):
+    if i == 5:
+        print("Five")
+    else:
+        print("i")
---
+++ Nov/ex_16112024_Loops/Lab059_Continue.py
@@ -0,0 +1,13 @@
+for num in range(1,100):
+    if num % 2 == 0:
+        print(num)
+        continue
+    else:
+        pass
+
+# | i  | Condition | O/P
+# | 0  | 0%2 == 0 -> True | continue - skip No O/P
+# | 1  | 1%2 == 0 -> False | 1
+
+
+# pass - can be used in the statement, fucntions, classes and objects.
